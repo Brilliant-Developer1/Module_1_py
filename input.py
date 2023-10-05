182a7e1b635e15b76a0683690dd20bbe3279121f
Brilliant-Developer1 <email2obail@gmail.com>
2023-10-05 19:32:02 +0600

    first commit

---
+++ input.py
@@ -0,0 +1,7 @@
+print("Input Here")
+input1 = input("Input Here: ")
+input2 = input("Input 2: ")
+total = int(input1) + int(input2)
+
+print("Here it is Total: " , total)
+
---
+++ test.py
@@ -0,0 +1,17 @@
+
+name ="Abid"
+age=29
+address = "Gazipur"
+text= f"Obaidullah Age is {age} and living in {address}"
+
+print(name)
+print(age)
+print(text)
+print(name +  " " + address)
+
+print(106/3)
+print(106//3)
+
+""" 
+Alt shift A
+"""
