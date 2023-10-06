f56f8c6373b8fe2326d84d35fb101df687465220
Brilliant-Developer1 <email2obaidul@gmail.com>
2023-10-06 22:07:45 +0600

    Practice

--- test.py
+++ test.py
@@ -1,16 +1,24 @@
 
 name ="Abid"
 age=29
+age2=30
 address = "Gazipur"
 text= f"Obaidullah Age is {age} and living in {address}"
 
+
+# for item in range:
+#     print(item)
+for i in range(1,10+1):
+    print(i)
+  
 print(name)
-print(age)
-print(text)
+if age < age2:
+    print(age2)
+print(5%3)
 print(name +  " " + address)
 
-print(106/3)
-print(106//3)
+# print(106/3)
+# print(106//3)
 
 """ 
 Alt shift A
---
+++ xyz.py
@@ -0,0 +1,19 @@
+x = 7
+if x > 5:
+    print("Greater than 5")
+else:
+    print("Less than or equal to 5")
+
+
+for i in range(5+1):
+    print(i)
+print("new")
+i = 0
+while i < 5:
+    print(i)
+    i+=1    
+
+print("new")
+x = 10
+y = 2
+print(x // y)    
