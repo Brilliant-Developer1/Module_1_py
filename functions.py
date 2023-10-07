ffd798191e420260d78cef61658cd70973df2a3f
Brilliant-Developer1 <email2obaidul@gmail.com>
2023-10-07 22:48:43 +0600

    Practice

--- functions.py
+++ functions.py
@@ -5,4 +5,22 @@ def double_it(num):
  return res
 
 ans = double_it(10)
-print(ans)
+# print(ans)
+
+#args
+def allSum(*numbers):
+ for num in numbers:
+  print(num)
+  
+ return numbers
+ 
+total = allSum(40,45,50) 
+print(total)
+
+def ful_name(first,last):
+ name = f'Full name is: {first} {last}'
+ return name
+# take as perameter
+# name=ful_name("Hello","Juii")
+name=ful_name(last = "Juii", first="Hi")
+print(name)
